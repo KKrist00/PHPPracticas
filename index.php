a3d459e06b11ea5612bb787de7cad94f2b15e7ba
kkrist00 <kristo.perez@gmail.com>
2018-01-10 08:44:50 +0000

    Segundo commmit

--- index.php
+++ index.php
@@ -5,4 +5,18 @@
  * Date: 10/01/2018
  * Time: 8:23
  */
+
+
 echo "Bienvenidos Php";
+
+<html>
+    <body>
+
+        <form action="PHPPracticas.php" method="post">
+            Nombre: <input type="text" name="nombre"><br>
+            E-mail: <input type="text" name="email"><br>
+                <input type="submit"> enviaR
+        </form>
+
+    </body>
+</html>
